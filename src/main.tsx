073818c0920663f97d17b1f7b107bd4aabf4a4a0
jean <jeansud@hotmail.com>
2024-03-06 23:59:10 -0300

    start

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Tetris from './component/Tetris.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+  <div>
+    <h1>Tetris Game</h1>
+    <Tetris />
+  </div>
+  </React.StrictMode>,
+)
